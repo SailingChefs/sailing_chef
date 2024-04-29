10c0213a50922ede1eeb143164f13e136366587c
Hafsa191097 <hafsaayyhere@gmail.com>
2024-04-29 14:53:15 +0500

    maps filter

--- lib/ui/views/pin_drop_map/widgets/searchbar.dart
+++ lib/ui/views/pin_drop_map/widgets/searchbar.dart
@@ -1,4 +1,3 @@
-import 'package:sailing_chefs/app/app.dialogs.dart';
 import 'package:sailing_chefs/core/helpers/capitalize_first_fucntion.dart';
 import 'package:sailing_chefs/core/imports/core_imports.dart';
 import 'package:sailing_chefs/model/pin_model.dart';
@@ -6,7 +5,6 @@ import 'package:sailing_chefs/services/search_service.dart';
 
 class PinsSearchDelegate extends SearchDelegate<List<PinnedLocation>> {
   final SearchService _searchService = SearchService();
-  final DialogService _navigationService = DialogService();
   @override
   String get searchFieldLabel => 'Search Pinned Locations';
 
